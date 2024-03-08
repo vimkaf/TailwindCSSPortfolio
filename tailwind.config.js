57d4588051b0f35a293ae2f692e17c59f94ec60b
ThaOracle <vimkaf@gmail.com>
2024-03-08 21:16:53 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./*.{html,js}'],
+  theme: {
+    colors: {
+      "primary" : "#079211",
+      "white" : "#FFFFFF",
+      "light-gray" : "#D1D5DB",
+      "gray" : "#808080",
+      "deep-gray" : "#0A0A0F",
+      "light-dark" : "#313131",
+      "dark" : "#101017",
+      "deep-dark" : "#050507"
+    },
+    fontFamily: {
+      sans: ['Inter'],
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
+
